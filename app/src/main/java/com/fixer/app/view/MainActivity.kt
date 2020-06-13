31103af31a1d9551aa710ca1f2161f1408934baa
creativeapps <creativeapps2020@gmail.com>
2020-06-13 22:01:51 +0300

    initial commit

---
+++ app/src/main/java/com/fixer/app/view/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fixer.app.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fixer.app.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
